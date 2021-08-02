0437381478647c7afda71b54deb0426e208ae2e4
Salman Khan <salmankhan@xmos.com>
2021-08-02 15:25:26 +0100

    add function to map strings from context into one of the enums

--- src/authorisation.ts
+++ src/authorisation.ts
@@ -15,3 +15,45 @@ export enum commentAuthorAssoc {
     OWNER = 'OWNER',
 }
 
+// returns one of the associations from the string in the context
+export function getCommentAuthorAssoc(
+    comment: {[key: string]: any; id: number} | undefined
+): commentAuthorAssoc {
+    if (comment === undefined)
+        throw new Error('context.payload.comment is undefined.');
+
+    let assoc: commentAuthorAssoc;
+
+    const author_association = comment.author_association;
+
+    switch (author_association) {
+        case 'COLLABORATOR':
+            assoc = commentAuthorAssoc.COLLABORATOR;
+            break;
+        case 'CONTRIBUTOR':
+            assoc = commentAuthorAssoc.CONTRIBUTOR;
+            break;
+        case 'FIRST_TIMER':
+            assoc = commentAuthorAssoc.FIRST_TIMER;
+            break;
+        case 'FIRST_TIME_CONTRIBUTOR':
+            assoc = commentAuthorAssoc.FIRST_TIME_CONTRIBUTOR;
+            break;
+        case 'MANNEQUIN':
+            assoc = commentAuthorAssoc.MANNEQUIN;
+            break;
+        case 'MEMBER':
+            assoc = commentAuthorAssoc.MEMBER;
+            break;
+        case 'NONE':
+            assoc = commentAuthorAssoc.NONE;
+            break;
+        case 'OWNER':
+            assoc = commentAuthorAssoc.OWNER;
+            break;
+        default:
+            throw new Error(`Unrecognised user association: ${author_association}`);
+    }
+    return assoc;
+}
+
